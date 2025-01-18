7ecc8fdc850e85f3d68d6d4c7750662e06373b27
Jonatan <kalinowskijonatan41@gmail.com>
2025-01-18 10:38:39 +0100

    Move square declaration into loop
    Square was created before the loop, what caused confusion while
    setting the grid size. It looked like there is one more square in
    a grid than declared.
    
    Now square is created in a loop.

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
 const grid = document.querySelector('#grid');
 const square = document.createElement('div');
+const gridSize = document.createElement('input');
 
-grid.appendChild(square);
-square.className = 'squares';
+
+
+// grid.appendChild(square);
+// square.className = 'squares';
 
 // grid.appendChild(square2);
 // square2.className = "squares";
 // console.log(square.className);
 
-for ( let i = 1; i <= 255; i++) {
-    grid.appendChild(square.cloneNode());    
+// for ( let i = 1; i <= 255; i++) {
+//     grid.appendChild(square.cloneNode());    
+// }
+
+for ( let i = 0; i < 256; i++) {
+
+    const square = document.createElement('div');
+    square.setAttribute('id', `square${i}`);
+    square.setAttribute('class', 'squares');
+    grid.appendChild(square);
+    // grid.appendChild( document.createElement('div').className = 'squares' );
+    // grid.appendChild(square.cloneNode());    
 }
+
 
 const squaresNodes = document.querySelectorAll('.squares');
 console.log(squaresNodes);
