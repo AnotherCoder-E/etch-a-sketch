27c9e9355fb76b5c6172423a60eb1e4d3e7bd405
Jonatan <kalinowskijonatan41@gmail.com>
2025-01-18 12:49:13 +0100

    Allow user set the grid size
    Grid size was set to hard-coded value 16x16 of 50px squares.
    Now user can set the size of grid by prompt by providing amount of
    squares per line.

--- script.js
+++ script.js
@@ -1,37 +1,59 @@
+const body = document.querySelector('body');
 const grid = document.querySelector('#grid');
 const square = document.createElement('div');
-const gridSize = document.createElement('input');
+const gridSize = document.createElement('button');
 
+gridSize.textContent = 'Change Grid size';
+gridSize.className = 'gridSize';
 
+gridSize.addEventListener('click', () => {
+    
+    while (true) {
+    const userInput = prompt('Set number of squares per line (up to 100): ');
+    console.log('User Input is: ' + userInput + '\n It\'s type is: ' + typeof userInput);
+    if ( isNaN(userInput) || userInput === '') {
+        alert('Wrong input. Provide correct one or press Cancel.');
+    } else if ( userInput === null ) {
+        alert('Canceled');
+        return false;
+    } else {
+        grid.innerHTML = ''; // make that cleaning grid function
+        createGrid(parseInt(userInput));
+        return false;
+    }
+    }
 
-// grid.appendChild(square);
-// square.className = 'squares';
-
-// grid.appendChild(square2);
-// square2.className = "squares";
-// console.log(square.className);
-
-// for ( let i = 1; i <= 255; i++) {
-//     grid.appendChild(square.cloneNode());    
-// }
-
-for ( let i = 0; i < 256; i++) {
-
-    const square = document.createElement('div');
-    square.setAttribute('id', `square${i}`);
-    square.setAttribute('class', 'squares');
-    grid.appendChild(square);
-    // grid.appendChild( document.createElement('div').className = 'squares' );
-    // grid.appendChild(square.cloneNode());    
-}
-
-
-const squaresNodes = document.querySelectorAll('.squares');
-console.log(squaresNodes);
-
-squaresNodes.forEach( (square) => {
-    square.addEventListener('mouseover', () => {
-        console.log(square);
-        square.style.backgroundColor = 'green';
-    });
 });
+
+body.insertBefore(gridSize, grid);
+
+
+function createGrid(numOfSquares) {
+    // TEST
+    alert('createGrid function says \nUser Input is: ' + numOfSquares + '\n It\'s type is: ' + typeof numOfSquares);
+    // END TEST
+
+    const squaresAmount = numOfSquares**2; // power it
+    const squareSize = String( 800 / numOfSquares ) + 'px';
+    
+    for ( let i = 0; i < squaresAmount; i++) {
+
+        const square = document.createElement('div');
+        square.setAttribute('id', `square${i}`);
+        square.setAttribute('class', 'squares');
+        square.style.width = squareSize;
+        square.style.height = squareSize;
+        grid.appendChild(square);
+    }
+
+    const squaresNodes = document.querySelectorAll('.squares');
+    console.log(squaresNodes);
+
+    squaresNodes.forEach( (square) => {
+        square.addEventListener('mouseover', () => {
+            console.log(square);
+            square.style.backgroundColor = 'green';
+        });
+    });
+
+}
